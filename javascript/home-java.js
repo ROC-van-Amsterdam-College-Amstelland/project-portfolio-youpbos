ab518f9b188beba5e3883b40975aca7a4ce0f083
youp bos <youpbos121@gmail.com>
2019-11-05 09:53:25 +0100

    javacript foto's, alles opnieuw gelinked

---
+++ javascript/home-java.js
@@ -0,0 +1,10 @@
+function verander1(){
+document.getElementById("mijzelf1").style.display= "none";
+document.getElementById("mijzelf2").style.display= "block";
+
+}
+function verander2(){
+    document.getElementById("mijzelf1").style.display= "block";
+    document.getElementById("mijzelf2").style.display= "none";
+    
+    }
